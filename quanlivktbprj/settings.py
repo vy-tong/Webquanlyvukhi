1ca021c67bb6acdf56af9e3d8a7d79419cd89f6b
quocytihn <quoc250124@gmail.com>
2024-11-14 09:49:48 +0700

    Setting.py

--- quanlivktbprj/settings.py
+++ quanlivktbprj/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'quanlivktb',
         'USER' : 'root',
-        'PASSWORD' : '',
+        'PASSWORD' : '1234',
         'HOST' : 'localhost',
         'PORT' : '3306'
     }
